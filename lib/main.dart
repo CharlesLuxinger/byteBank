34661f467db9aecb614a72b345a8cf6d3bf1c710
Charles Luxinger <charles.luxinger@picpay.com>
2020-04-14 15:40:52 -0300

    4. Refatorando o código

--- lib/main.dart
+++ lib/main.dart
@@ -6,20 +6,47 @@ void main() => runApp(
           appBar: AppBar(
             title: Text("Transferência"),
           ),
-          body: Column(
-            children: <Widget>[
-              Card(
-                child: ListTile(
-                  leading: Icon(Icons.monetization_on),
-                  title: Text("Teste"),
-                  subtitle: Text("SubTeste"),
-                ),
-              ),
-            ],
-          ),
+          body: TransfersList(),
           floatingActionButton: FloatingActionButton(
             child: Icon(Icons.add),
           ),
         ),
       ),
     );
+
+class TransfersList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        TransferItem(Transfer(1234, 100.0)),
+        TransferItem(Transfer(1235, 200.0)),
+        TransferItem(Transfer(1236, 300.0)),
+      ],
+    );
+  }
+}
+
+class TransferItem extends StatelessWidget {
+  final Transfer _transfer;
+
+  TransferItem(this._transfer);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        leading: Icon(Icons.monetization_on),
+        title: Text(_transfer._value.toString()),
+        subtitle: Text(_transfer._account.toString()),
+      ),
+    );
+  }
+}
+
+class Transfer {
+  final int _account;
+  final double _value;
+
+  Transfer(this._account, this._value);
+}
